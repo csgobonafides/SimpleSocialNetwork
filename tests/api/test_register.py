d629fe79a0a6c4cb69daf847aca522194920a544
bonafides <alexsandr.ejik@gmail.com>
2025-06-20 18:21:22 +0500

    two tests

--- tests/api/test_register.py
+++ tests/api/test_register.py
@@ -3,9 +3,6 @@ import pytest
 from httpx import AsyncClient
 from datetime import datetime
 
-from db.connector import DataBaseConnector
-from social_page.schemas import SocialPageRequest
-
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("prepare_social")
@@ -22,3 +19,44 @@ async def test_register_201(xclient: AsyncClient):
     }
     response = await xclient.post("/social_page/user/register", json=payload)
     assert response.status_code == 201, response.text
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("prepare_social")
+async def test_register_403(xclient: AsyncClient):
+    payload = {
+        "login": "test1",
+        "password": "ttest",
+        "first_name": "ttest",
+        "last_name": "ttest",
+        "data_of_birth": str(datetime.now().date()),
+        "gender": "O",
+        "interests": "ttest",
+        "city": "Ugansk"
+    }
+    response = await xclient.post("/social_page/user/register", json=payload)
+    assert response.status_code == 403, response.text
+    assert response.json() == {"detail": "Such user is already registered."}
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("prepare_social")
+async def test_register_422(xclient: AsyncClient):
+    payload = {
+        "login": "Vika",
+        "password": 123,
+        "first_name": "ttest",
+        "last_name": "ttest",
+        "data_of_birth": str(datetime.now().date()),
+        "gender": "O",
+        "interests": "ttest",
+        "city": "Ugansk"
+    }
+    response = await xclient.post("/social_page/user/register", json=payload)
+    assert response.status_code == 422, response.text
+    assert response.json() == {"detail": [
+        {"type": "string_type",
+         "loc": ["body","password"],
+         "msg": "Input should be a valid string",
+         "input": 123}
+    ]}
